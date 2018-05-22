a805941cb46ca8a40dd2bf658a87198e71fc30fe
RTtenedero <36413776+RTtenedero@users.noreply.github.com>
2018-05-22 11:05:34 -0400

    Update ZombieArray.java

--- ZombieArray.java
+++ ZombieArray.java
@@ -1,12 +1,12 @@
-ArrayList<zombie>Dead;
+ArrayList<Zombie>zombies;
 int ZombieWidth = 48;
 
 void setup() {
 size(500, 500);
 noStroke();
 
-Dead = new ArrayList<zombie>();
-Dead.add(new zombie(width/2, 0, ZombieWidth));
+zombies = new ArrayList<Zombie>();
+zombies.add(new Zombie(width/2, 0, ZombieWidth));
 }
 void draw() {
   
